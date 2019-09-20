4c2a208f0c00f2576c736251832e94cdcb3f50ad
Rishika1983 <35499575+Rishika1983@users.noreply.github.com>
2019-09-20 04:11:12 -0700

    Add files via upload

---
+++ exponentfunc.py
@@ -0,0 +1,10 @@
+
+def raised_to_power(num, power):
+    base=num
+    for index in range(power):
+        if index == 0:
+            num = num*1
+        else:
+            num = num*base
+    return num
+print(raised_to_power(2, 3))
