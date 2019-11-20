8e359b88d6f4d7accc24e103b99616888fe24915
Rishika1983 <35499575+Rishika1983@users.noreply.github.com>
2019-11-20 03:23:27 -0800

    Practice of For and While loop

---
+++ loopPractice.py
@@ -0,0 +1,16 @@
+Genres=[ 'rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop']
+for gen in Genres:
+    print(gen)
+
+# Write a for loop that prints out the following list: squares=['red', 'yellow', 'green', 'purple', 'blue']
+
+print("Write a for loop that prints out the following list: squares=['red', 'yellow', 'green', 'purple', 'blue'")
+squares=['red', 'yellow', 'green', 'purple', 'blue']
+for col in squares:
+    print(col)
+
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i=0
+while (PlayListRatings[i]>6):
+    print(PlayListRatings[i])
+    i = i+1
